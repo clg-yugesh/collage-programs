1a8ea3921d0b565386f8a353e228655654011f4a
Yugesh A <a.yugesh2@gmail.com>
2024-11-24 21:37:06 +0530

    Delete conditional-programs directory

--- conditional-programs/1.positive-or-not.py
+++
@@ -1,9 +0,0 @@
-#input
-a = int(input("Enter a number : "))
-
-if a == 0:
-    print("The given numbers is 0")
-elif a > 0:
-    print("The given number is positive")
-else: 
-    print("The given number is negative")
--- conditional-programs/7.factorial.py
+++
@@ -1,9 +0,0 @@
-#input
-num = int(input("Enter a number : "))
-#inizilation
-fact = 1
-#looping
-for i in range(1,num + 1):
-    fact = i * fact
-#output
-print("The factorial of",num,"is",fact)
